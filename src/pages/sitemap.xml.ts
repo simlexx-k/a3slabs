4e3f7b4c75cbbaac870b8ee6debef868976ab170
Simon Kosgei <simonwriter254@gmail.com>
2026-08-22 11:24:44 +0300

    Add brand system to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,22 +3,8 @@ import { products } from "../data/products";
 import { caseStudies } from "../data/caseStudies";
 
 export const prerender = true;
-
 export const GET: APIRoute = () => {
-  const paths = [
-    "/",
-    "/products",
-    "/work",
-    "/research",
-    "/engineering",
-    "/builds",
-    "/about",
-    "/contact",
-    ...products.map((product) => `/products/${product.slug}`),
-    ...caseStudies.map((study) => `/case-studies/${study.slug}`),
-  ];
+  const paths = ["/", "/products", "/work", "/research", "/engineering", "/builds", "/brand", "/about", "/contact", ...products.map((product) => `/products/${product.slug}`), ...caseStudies.map((study) => `/case-studies/${study.slug}`)];
   const urls = paths.map((path) => `<url><loc>${new URL(path, "https://a3slabs.co.ke").toString()}</loc></url>`).join("");
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, {
-    headers: { "Content-Type": "application/xml; charset=utf-8", "Cache-Control": "public, max-age=3600" },
-  });
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, {headers:{"Content-Type":"application/xml; charset=utf-8","Cache-Control":"public, max-age=3600"}});
 };
